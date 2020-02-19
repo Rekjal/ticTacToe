483ff971cdeb49b81511ba6b7194b40538c4507a
Rekjal <32828154+Rekjal@users.noreply.github.com>
2020-02-18 18:13:15 -0800

    Copied updated content from Daimler laptop

--- scripts.js
+++ scripts.js
@@ -1,269 +1,133 @@
- $(document).ready(function () {
-    $('.game-restart').click(function (e) { 
-      document.location.reload(true);
-     });
-// $('cell').each(function(el){
-//   if(el == ''){
-    var player1;
-    var player2;
-    var gameOver = false;
-  
-    $('.hide').hide()
-    $('.game-restart').hide()
-   $('#start-game').click(function (e) { 
-     $('#start-game').hide();
-      e.preventDefault();
-      $('.hide').show()
-      startGame();
+var player1;
+var player2;
+var gameOver = false;
+
+function Player(playerName, playSymbol, winStatus, activeUser, boardStatus) { //constructor/blue print looks like an object
+    this.playerName = playerName;
+    this.playSymbol = playSymbol;
+    this.winStatus = winStatus;
+    this.activeUser = activeUser;    
+    this.boardStatus = ['', '', '', '', '', '', '', '', ''];
+}
+
+function randomNumGen() {
+    return Math.floor((Math.random() * 9)); //Generate Random no from 0 to 8
+};
+
+
+
+var board = ['', '', '', '', '', '', '', '', ''];
+
+$(document).ready(function () {
+    $('.game-restart').click(function (e) {   //Restart game button
+        document.location.reload(true);       //Reload Page
     });
 
+    $('.hide').hide()           //Hide Grid
+    $('.game-restart').hide()   //Hide restart game button
 
-    startGame = function() {
-     player1 = prompt('Please choose X or O').toUpperCase();
-      if(player1 === 'X'){
-        player2 = 'O'
-      } else {
-        player2 = 'X'
-      };
+    $('#start-game').click(function (e) {  //Show Starts here. Why e?
+        $('#start-game').hide();           //Hide start game button
+        e.preventDefault();
+        $('.hide').show()
+        startGame();
+    });
+
+    var player1 = new Player("Player-1", "X", false, ['', '', '', '', '', '', '', '', ''], false);
+    var player2 = new Player("Player-2", "O", false, ['', '', '', '', '', '', '', '', ''], false);
+
+    function startGame() {
+        player1.playSymbol = prompt('Please choose X or O').toUpperCase();
+        if (player1.playSymbol === 'X') {
+            player2.playSymbol = 'O'
+        } else {
+            player2.playSymbol = 'X'
+        };
     }
 
-  
-    
-
-    var randomNo = function() {
-      return Math.floor((Math.random() * 8) + 1); //R&&om no generat|| from 1-8.
+    function nonClickable() {
+        if (player1.winStatus === true || player2.winStatus === true) {
+            $('.cells').addClass('unClick');
+            
+        }
     };
 
-    var board = ['','','','','','','','',''];
-
-    var computerTurn = function(){
-      var space = randomNo();
-      while(board[space] !== '') {
-          space = randomNo();
-          console.log(`Inside while board is ${board[space]} and random number is ${space}`);
+    function computerTurn() {
+        var cellLocation = randomNumGen();
+        while (board[cellLocation] !== '') {
+            cellLocation = randomNumGen();
+            console.log(`      Inside While Loop: Random number is ${cellLocation}`);
         }
-            console.log("space is " + space);
-          $('#' +  space).append(player2);
-          board[space] = player2;
-          checkPlayer2Win();
+        console.log(`      Computer chosen UNOCCUPIED cellLocation is ${cellLocation}`);
+        $('#' + cellLocation).append(player2.playSymbol);
+        board[cellLocation] = player2.playSymbol;
+        player2.boardStatus[cellLocation] = player2.playSymbol;
+        checkPlayerWin();
     }
-  
-    const checkPlayer1Win = function(){
-      if(
-      ((board[6] == player1 && board[7] == player1 && board[8] == player1) || // bottom row
-      (board[3] == player1 && board[4] == player1 && board[5] == player1) || // across the middle
-      (board[0] == player1 && board[1] == player1 && board[2] == player1) || // across the bottom
-      (board[0] == player1 && board[3] == player1 && board[6] == player1) || // down the middle
-      (board[1] == player1 && board[4] == player1 && board[7] == player1) || // down the middle
-      (board[2] == player1 && board[5] == player1 && board[8] == player1) || // down the right side
-      (board[0] == player1 && board[4] == player1 && board[8] == player1) || // diagonal
-      (board[6] == player1 && board[4] == player1 && board[2] == player1) ) ) // bottom row
-      
-    {
-      console.log('Player1 You have won!');
-      $('.game-restart').show();
-       gameOver = true;
+
+    function checkPlayerWin() {
+        if (
+            ((board[6] == player1.playSymbol && board[7] == player1.playSymbol && board[8] == player1.playSymbol) || // bottom row
+             (board[3] == player1.playSymbol && board[4] == player1.playSymbol && board[5] == player1.playSymbol) || // middle row
+             (board[0] == player1.playSymbol && board[1] == player1.playSymbol && board[2] == player1.playSymbol) || // top row
+             (board[0] == player1.playSymbol && board[3] == player1.playSymbol && board[6] == player1.playSymbol) || // down the left
+             (board[1] == player1.playSymbol && board[4] == player1.playSymbol && board[7] == player1.playSymbol) || // down the middle
+             (board[2] == player1.playSymbol && board[5] == player1.playSymbol && board[8] == player1.playSymbol) || // down the right
+             (board[0] == player1.playSymbol && board[4] == player1.playSymbol && board[8] == player1.playSymbol) || // diagonal
+             (board[6] == player1.playSymbol && board[4] == player1.playSymbol && board[2] == player1.playSymbol)))  // diagonal
+        {            
+            $('.game-restart').show();
+            player1.winStatus = true;
+            alert(player1.playerName + " You have won!!!!");
+            console.log(`WIN Loop: Player1 array playerName:playSymbol:winStatus:boardStatus::::${player1.playerName}:${player1.playSymbol}:${player1.winStatus}:${player1.boardStatus}`);
+            nonClickable();
+        }
+       else if (
+            ((board[6] == player2.playSymbol && board[7] == player2.playSymbol && board[8] == player2.playSymbol) || // bottom row
+             (board[3] == player2.playSymbol && board[4] == player2.playSymbol && board[5] == player2.playSymbol) || // middle row
+             (board[0] == player2.playSymbol && board[1] == player2.playSymbol && board[2] == player2.playSymbol) || // top row
+             (board[0] == player2.playSymbol && board[3] == player2.playSymbol && board[6] == player2.playSymbol) || // down the left
+             (board[1] == player2.playSymbol && board[4] == player2.playSymbol && board[7] == player2.playSymbol) || // down the middle
+             (board[2] == player2.playSymbol && board[5] == player2.playSymbol && board[8] == player2.playSymbol) || // down the right
+             (board[0] == player2.playSymbol && board[4] == player2.playSymbol && board[8] == player2.playSymbol) || // diagonal
+             (board[6] == player2.playSymbol && board[4] == player2.playSymbol && board[2] == player2.playSymbol)))  // diagonal
+        {
+            $('.game-restart').show();
+            player2.winStatus = true;
+            alert(player2.playerName + " You have won!!!!");            
+            console.log(`WIN Loop: Player2 array playerName:playSymbol:winStatus:boardStatus::::${player2.playerName}:${player2.playSymbol}:${player2.winStatus}:${player2.boardStatus}`);
+            nonClickable();
+        }
+        else {
+            console.log(`      Non-WIN Loop: Player1 array playerName:playSymbol:winStatus:boardStatus::::${player1.playerName}:${player1.playSymbol}:${player1.winStatus}:${player1.boardStatus}`);
+            console.log(`      Non-WIN Loop: Player2 array playerName:playSymbol:winStatus:boardStatus::::${player2.playerName}:${player2.playSymbol}:${player2.winStatus}:${player2.boardStatus}`);
+        }
     }
-    } 
-
-    const checkPlayer2Win = function(){
-      if(
-        ((board[6] == player2 && board[7] == player2 && board[8] == player2) || // bottom row
-        (board[3] == player2 && board[4] == player2 && board[5] == player2) || // across the middle
-        (board[0] == player2 && board[1] == player2 && board[2] == player2) || // across the bottom
-        (board[0] == player2 && board[3] == player2 && board[6] == player2) || // down the middle
-        (board[1] == player2 && board[4] == player2 && board[7] == player2) || // down the middle
-        (board[2] == player2 && board[5] == player2 && board[8] == player2) || // down the right side
-        (board[0] == player2 && board[4] == player2 && board[8] == player2) || // diagonal
-        (board[6] == player2 && board[4] == player2 && board[2] == player2)) ) 
-    {
-      console.log('Player 2 You have won!');
-      $('.game-restart').show();
-       gameOver = true;
-    }
-    } 
-
-      $('.cell').each(function(el){
-        $('#' + el).click(function(){
-            console.log("click deteeted");
-          if(board[el] == ''){
-            $('#' + el).append(player1);
-            board[el] = player1;
-            console.log("player1 is " + player1);
-            console.log("player2 is " + player2);
-            checkPlayer1Win();
-            // checkPlayer2Win();
-            if(board.includes('')){
-              if(gameOver == false){
-                setTimeout(function(){
-                  computerTurn();
-
-                }, 800);
-              }
-            }       
-         }
-            console.log(board);
-            })
+    
+    $('.cells').each(function (cell) {
+        $('#' + cell).click(function () {
+            //console.log("Click Detected");
+            if ((board[cell] == '') && (player1.winStatus === false) && (player2.winStatus === false) ){
+                $('#' + cell).append(player1.playSymbol);
+                board[cell] = player1.playSymbol;
+                player1.boardStatus[cell] = player1.playSymbol; 
+                player1.activeUser = true;                  
+                console.log(`Main loop: Player1 array playerName:playSymbol:winStatus:boardStatus::::${player1.playerName}:${player1.playSymbol}:${player1.winStatus}:${player1.boardStatus}`);
+               // setTimeout(function () {
+                    checkPlayerWin(); 
+               // }, 100);      
+                if (board.includes('')) {
+                    if ((player1.winStatus === false) && (player2.winStatus === false)) {
+                        console.log("Calling ComputerTurn")
+                        player1.activeUser = false;
+                        player2.activeUser = true;
+                        setTimeout(function () {
+                            computerTurn();
+                        }, 800); 
+                    }
+                }
+            }
+            //console.log(board);
         })
-      
-      
-      
-
-        
-     
-
-    });
-
-
-
-
-    // playGame = () => {
-    //   $('.cell').each(function(el){
-    //     $('#' + el).click(function(){
-    //       if(!board[el]){
-    //         $('#' + el).append(player1);
-    //         board[el] = player1;
-    //         console.log("player1 is " + player1);
-    //         console.log("player2 is " + player2);
-    //         if(board.includes('')){
-    //           computerTurn();
-    //         }          
-    //      }
-    //         console.log(board);
-    //         })
-    //     })
-    //   }
-
-      // old class syntax
-      // class Player {
-      //   constructor(player1){
-      //     this.player1 = player1;
-      //   }
-      
-      //   player1() {
-      //     console.log(this.player1);
-      //   }
-      // }
-      
-      // class Space {
-      //   constructor(options){
-    
-      //   }
-      // }
-      // // board logic
-      // class Board {
-      //   constructor(){
-      //   this.board = ['','','','','','','','',''];
-      //   };
-      
-      //   drawBoard(){
-      //     $('.board').append(this.board);
-      //   }
-      // }
-    
-      // class Game {
-      //   constructor(){
-      //     this.board = new Board();
-      //     this.players = [
-      //       new Player("x"),
-      //       new Player("o")
-      //     ]
-      //   }
-      // }
-
-      // old play syntax (before loop)
-
-    // $('#0').click(function(){
-    //   if(!board[0]){
-    //   $('#0').append(player1);
-    //   board[0] = player1;
-    //   console.log(player1);
-    //   console.log(player2);
-    //   computerTurn();
-    //   }
-    // })
-    // $('#1').click(function(){
-    //   if(!board[1]){
-    //   $('#1').append(player1);
-    //   board[1] = player1;
-    //   computerTurn();
-
-    //   }
-    // })
-    // $('#2').click(function(){
-    //   if(!board[2]){
-    //   $('#2').append(player1);
-    //   board[2] = player1;
-    //   computerTurn();
-
-    //   }
-    // })
-    // $('#3').click(function(){
-    //   if(!board[3]){
-    //   $('#3').append(player1);
-    //   board[3] = player1;
-    //   computerTurn();
-
-    //   }
-    // })
-    // $('#4').click(function(){
-    //   if(!board[4]){
-    //   $('#4').append(player1);
-    //   board[4] = player1;
-    //   computerTurn();
-
-    //   }
-    // })
-    // $('#5').click(function(){
-    //   if(!board[5]){
-    //   $('#5').append(player1);
-    //   board[5] = player1;
-    //   computerTurn();
-
-    //   }
-    // })
-    // $('#6').click(function(){
-    //   if(!board[6]){
-    //   $('#6').append(player1);
-    //   board[6] = player1;
-    //   computerTurn();
-
-    //   }
-    // })
-    // $('#7').click(function(){
-    //     if(!board[7]){
-    //     $('#7').append(player1);
-    //     board[7] = player1;
-    //     computerTurn();
-
-    //     }
-    //   })
-
-    //   $('#8').click(function(){
-    //     if(!board[8]){
-    //     $('#8').append(player1);
-    //     board[8] = player1;
-    //     computerTurn();
-
-    //     }
-      
-      
-
-    
-  
-    // else {
-    //   alert('err||')
-    // };
-  // });
-//$("#header").click(function () {
-    //$(".board").append(`${board1.board[0]}${board1.board[1]} ${board1.board[2]}${board1.board[3]}${board1.board[4]}<br>${board1.board[5]}${board1.board[6]} ${board1.board[7]}${board1.board[8]}${board1.board[9]}<br>${board1.board[10]}${board1.board[11]} ${board1.board[12]}${board1.board[13]}${board1.board[14]}<br>
-
-
-
-
-// });
-
-
-
-
- 
+    })
+});
